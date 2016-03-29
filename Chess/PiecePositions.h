a3ce785ca9001d76a55b234ee714a66ca5b809cd
sh999 <sh999gh@gmail.com>
2016-03-29 17:43:09 -0400

    Edit comments

--- Chess/PiecePositions.h
+++ Chess/PiecePositions.h
@@ -8,36 +8,34 @@
 using namespace std;
 class PiecePositions{
 /*
+ Summary:
+ =======
  Parses FEN-type notation for setting position of pieces
  See: https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation#Definition
- e.g. starting chess is given by:
+ e.g. normal starting chess position is given by:
      rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
  I will implement the string of pieces and not the things after 'w', which are extra information
  for a complete game. For now, just parse the string to set a normal table
 
  Input:
+ =====
     FEN-type string such as 
- rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
+ 		rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
     letters represent piece type (obvious). Numbers are # of empty squares
     '/' separates ranks. Start at 8th rank. Lowercase letters are black pieces
  */
 
 public:
     PiecePositions();
-    // Positions is a FEN-like string to
-    // describe piece position. Here, the constructor parses it and
-    // Creates Piece objects that are put in allPieces vector of pieces
-    PiecePositions(string positions);   
+    PiecePositions(string positions); // Positions is a FEN-like string to   
+    				      // describe piece position. Here, the constructor parses it and
+    				      // creates Piece objects that are put in allPieces vector of pieces */
     vector<Piece*> getPieces();
-    // After creating pieces, send information to board
-    void sendPiecesToBoard(Board &b);
+    void sendPiecesToBoard(Board &b); // After creating pieces, send information to board
 
 private:
-    // Holds all created pieces
-    vector<Piece *> allPieces;
-    // Converts square ID (0-62) to position string (e.g. e2)
-    string squareToPos(int i);
-
+    vector<Piece *> allPieces;	      // Holds all created pieces
+    string squareToPos(int i);	      // Converts square ID (0-62) to position string (e.g. e2)
 };
 
 
